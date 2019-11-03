dca30723055d760ec92e9186c3c6b372a37f07b1
Sweetum's Computer <Sweetum's Computer@DESKTOP-QGUPCF1>
2019-11-03 12:32:57 -0600

    Added Profile database and started comment feature

--- Hubbub/src/java/edu/acc/jee/hubbub/domain/Post.java
+++ Hubbub/src/java/edu/acc/jee/hubbub/domain/Post.java
@@ -1,12 +1,16 @@
 package edu.acc.jee.hubbub.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Post implements Serializable {
     private String content;
     private User   author;
     private Date   posted;
+    private Integer id;
+    private List<Comment> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -45,6 +49,22 @@ public class Post implements Serializable {
 
     public void setPosted(Date posted) {
         this.posted = posted;
+    }
+    
+    public List<Comment> getComments() {
+        return comments;
+    }
+    
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     @Override
