17c9a8c4272c7f331886768181253adbf6b11894
Sweetum's Computer <Sweetum's Computer@DESKTOP-QGUPCF1>
2019-11-03 12:39:17 -0600

    Added Profile database and started comment feature

--- Hubbub/src/java/edu/acc/jee/hubbub/domain/ListDAO1.java
+++ Hubbub/src/java/edu/acc/jee/hubbub/domain/ListDAO1.java
@@ -1,14 +1,17 @@
 package edu.acc.jee.hubbub.domain;
 
+import edu.acc.jee.hashtool.HashTool;
 import edu.acc.jee.hubbub.UserDTO;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class ListDao implements DataService {
+public class ListDAO1 implements DataService {
     private List<User> users = new ArrayList<>();
     private List<Post> posts = new ArrayList<>();
+    private List<Profile> profiles = new ArrayList<>();
+    private List<Comment> comments = new ArrayList<>();
 
     @Override
     public synchronized User addUser(UserDTO bean) {
@@ -16,7 +19,11 @@ public class ListDao implements DataService {
             throw new IllegalArgumentException("Bean does not validate");
         if (this.userExists(bean.getUsername()))
             throw new IllegalArgumentException("Username " + bean.getUsername() + " is unavailable");
-        User user = new User(bean.getUsername(), bean.getPassword());
+        String hash = HashTool.hash(bean.getPassword());
+        Profile profile = new Profile();
+        profile.setId(profile.hashCode());
+        profiles.add(profile);
+        User user = new User(bean.getUsername(), hash, profile);
         users.add(user);
         return user;
     }
@@ -26,9 +33,9 @@ public class ListDao implements DataService {
         if (!bean.isValid())
             throw new IllegalArgumentException("Bean does not validate");
         User user = this.findUserByUsername(bean.getUsername());
-        if (user != null && user.getPasshash().equals(bean.getPassword()))
-            return user;
-        else return null;
+        if (user == null) return null;
+        boolean match = HashTool.compare(bean.getPassword(), user.getPasshash());
+        return match ? user : null;
     }
 
     @Override
@@ -42,7 +49,7 @@ public class ListDao implements DataService {
     }
 
     @Override
-    public synchronized boolean userExists(String username) {
+    public boolean userExists(String username) {
         return this.findUserByUsername(username) != null;
     }
 
@@ -69,4 +76,31 @@ public class ListDao implements DataService {
                 .limit(limit)
                 .collect(Collectors.toList());
     }
+    
+    @Override
+    public Comment addComment(User author, Post target, String content) {
+        content = content
+                .replace("<", "&lt;")
+                .replace(">", "&gt;")
+                .replace("'", "&apos;")
+                .replace("\"", "&quot;")
+                .replace("%", "&#37;");    
+        Comment comment = new Comment(author, target, content);
+        comment.setId(comment.hashCode());
+        comments.add(comment);
+        target.getComments().add(comment);
+        return comment;
+    }
+    
+    @Override
+    public List<Comment> findCommentsByTargetAndPage(Post target, int offset, int limit) {
+        return target.getComments()
+                .stream()
+                .sorted((a,b) -> b.getCommented().compareTo(a.getCommented()))
+                .skip(offset)
+                .limit(limit)
+                .collect(Collectors.toList());        
+    }
 }
+
+//blank comment
