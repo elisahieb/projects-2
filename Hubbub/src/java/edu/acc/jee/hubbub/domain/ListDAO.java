f94a2ce7d6898edd6f9c888499996bb03473c74b
Sweetum's Computer <Sweetum's Computer@DESKTOP-QGUPCF1>
2019-11-03 12:40:31 -0600

    Added Profile database and started comment feature

--- Hubbub/src/java/edu/acc/jee/hubbub/domain/ListDAO.java
+++ Hubbub/src/java/edu/acc/jee/hubbub/domain/ListDAO.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class ListDAO1 implements DataService {
+public class ListDAO implements DataService {
     private List<User> users = new ArrayList<>();
     private List<Post> posts = new ArrayList<>();
     private List<Profile> profiles = new ArrayList<>();
